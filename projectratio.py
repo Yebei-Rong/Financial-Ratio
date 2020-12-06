7316759d54052a3ac482edfb80d2cd071d895050
Yebei Rong <65499719+Yebei-Rong@users.noreply.github.com>
2020-12-06 22:43:55 +0800

    Update projectratio.py

--- projectratio.py
+++ projectratio.py
@@ -15,7 +15,7 @@ average_receivable = (1880.5+138+1958.5)/2#(1880.5+1958.5)/2 #(????)
 COGS = 1955.4 # Cost of sales, excluding amortization and impairment of acquired intangible assets
 average_inventory = (804.2+929.9)/2
 purchase = 804.2-929.9+COGS
-average_trade_payables  = (530.8+ 370.5)/2#先用着account payable因为很多企业把trade payable和account payable合并了#为啥这公司没有呢 no vendor供货商吗
+average_trade_payables  = (530.8+ 370.5)/2
 revenue = 14377.9
 average_total_asset = (25288.9+27234.3+138)/2
 average_net_fixed_assets = (3247.3+3601.2)/2 #PPE
@@ -49,7 +49,7 @@ total_sharehold_equity = 13343.2+(2810.8*0.3)
 total_asset  = 27234.3+5.3-(3232.1-2999.83)
 average_total_equity = (13339.1+13031.6)/2
 EBIT = 7125.9
-interest_payment = 43.3 #last year,今年没有interest payment不合理
+interest_payment = 43.3 #last year
 lease_payment = 86.7 #in page42
 
 ###solvency ratio(financial ratio)
@@ -58,17 +58,17 @@ debt_to_capital = total_debt/(total_debt+total_sharehold_equity)
 debt_to_asset = total_debt/total_asset
 financial_leverage = average_total_asset/average_total_equity
 interest_coverage = EBIT/interest_payment
-fixed_charge_coverage = (EBIT+lease_payment)/(interest_payment+lease_payment) #只对大的航空公司影响大
+fixed_charge_coverage = (EBIT+lease_payment)/(interest_payment+lease_payment) 
 
-NI = 5993.3-5*0.16  #用的应该是comprehensive income which effect by avaiable for sales gain and loss
+NI = 5993.3-5*0.16  
 gross_profit = revenue-COGS 
-opeating_income = 7078.6 #Net cash flows provided by operating activities
+opeating_income = 7078.6 
 tax_rate = 16.25/100 
-average_total_capital = average_total_asset #简单点 = total asset ;total liability#Short Term Debt + Long Term Debt + Shareholder’s Equity
-#这家公司没有prefer stock
+average_total_capital = average_total_asset 
+
 ###profitability ratios
 net_profit_margin = NI/revenue
-gross_profit_margin = gross_profit/revenue#观察是否能通过来提高收入
+gross_profit_margin = gross_profit/revenue
 operating_profit_margin = opeating_income/revenue#/= EBIT/revenue
 ROA = (NI+interest_payment*(1-tax_rate))/(average_total_asset)
 operating_return_on_asset = opeating_income/average_total_asset
@@ -87,14 +87,14 @@ biogen_ratio['liquidity_ratio']=liquidity_ratio+list(np.zeros(5))
 biogen_ratio['solvency_ratio'] = solvency_ratio+list(np.zeros(3))
 biogen_ratio['profitability_ratios'] = profitability_ratios+list(np.zeros(2))
 
-
-'---------------------------------------------------------------------------------------------------------------'
+############################################################
+# For Year 2018
 annual_sales = 10886.8
 average_receivable = (1787.0+1958.5)/2 #(????)
 COGS = 1816.3 # Cost of sales, excluding amortization and impairment of acquired intangible assets
 average_inventory = (902.7+929.9)/2
 purchase = 804.2-929.9+COGS
-average_trade_payables  = (395.5+ 370.5)/2#先用着account payable因为很多企业把trade payable和account payable合并了#为啥这公司没有呢 no vendor供货商吗
+average_trade_payables  = (395.5+ 370.5)/2
 revenue = 13452.9
 average_total_asset = (25288.9+27234.3)/2
 average_net_fixed_assets = (3182.4+3601.2)/2 #PPE
@@ -129,7 +129,7 @@ total_sharehold_equity = 13031.6+(1636.2*0.3)
 total_asset  = 27234.3+5.3-(1636.2*0.075)
 average_total_equity = (12598.1+13031.6)/2
 EBIT = 5899.6
-interest_payment = 43.3 #last year,今年没有interest payment不合理
+interest_payment = 43.3 
 lease_payment = 60.2 #in page42
 
 ###solvency ratio(financial ratio)
@@ -138,17 +138,16 @@ debt_to_capital = total_debt/(total_debt+total_sharehold_equity)
 debt_to_asset = total_debt/total_asset
 financial_leverage = average_total_asset/average_total_equity
 interest_coverage = EBIT/interest_payment
-fixed_charge_coverage = (EBIT+lease_payment)/(interest_payment+lease_payment) #只对大的航空公司影响大
+fixed_charge_coverage = (EBIT+lease_payment)/(interest_payment+lease_payment) 
 
-NI = 4550.1-5*0.16  #用的应该是comprehensive income which effect by avaiable for sales gain and loss
+NI = 4550.1-5*0.16  
 gross_profit = revenue-COGS 
 opeating_income = 6187.7 #Net cash flows provided by operating activities
 tax_rate = 16.25/100 
-average_total_capital = average_total_asset #简单点 = total asset ;total liability#Short Term Debt + Long Term Debt + Shareholder’s Equity
-#这家公司没有prefer stock
+average_total_capital = average_total_asset # = total asset ;total liability#Short Term Debt + Long Term Debt + Shareholder’s Equity
 ###profitability ratios
 net_profit_margin = NI/revenue
-gross_profit_margin = gross_profit/revenue#观察是否能通过来提高收入
+gross_profit_margin = gross_profit/revenue#
 operating_profit_margin = opeating_income/revenue#/= EBIT/revenue
 ROA = (NI+interest_payment*(1-tax_rate))/(average_total_asset)
 operating_return_on_asset = opeating_income/average_total_asset
